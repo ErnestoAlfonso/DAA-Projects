959db20da8f3ba282ec75e27c19115f463b54ab1
jorgesolerrr <jorgeasoler2015@gmail.com>
2023-04-03 17:54:02 -0400

    new solution update

--- source.py
+++ source.py
@@ -9,84 +9,63 @@ class Student:
     def __repr__(self):
         return str(self.suspended) + ":" + str(self.group)
 
-def group(seq: list,  k : int):
-    if len(seq) == 0:
-        return seq
-    susp_grouped = []
-    i = 0
-    k_group = []
-    for stud_r in seq:
-        if i < k:
-            k_group.append(stud_r)
+def BuildMatrix(students: list[Student]):
+    amount_groups = 0
+    for student in students:
+        if student.group > amount_groups:
+            amount_groups = student.group
+    
+    matrix = [[0 for i in range(amount_groups)], 
+              [0 for i in range(amount_groups)]]
+    
+    for student in students:
+        if student.suspended == "P":
+            matrix[0][student.group-1] += 1
         else:
-            k1=k_group.copy()
-            susp_grouped.append(k1)
-            k_group.clear()
-            k_group.append(stud_r)
-            i = 0
-        i += 1
-    susp_grouped.append(k_group)
-    return susp_grouped
+            matrix[1][student.group-1] += 1
 
-def get_amount_rest(susp_grouped : list, k):
-    if len(susp_grouped) == 0:
-        return 0
-    a = len(susp_grouped[len(susp_grouped)-1])
-    return a if a<k else 0
+    return matrix
 
+def Solve(students: list[Student], k : int):
+    matrix = BuildMatrix(students)
+    amount_student_P = 0
+    amount_student_R = 0
+    for i in range(len(matrix[0])):
+        amount_student_P += matrix[0][i]
+        amount_student_R += matrix[1][i]
 
-def count_stud_per_group(students : list[Student]):
-    amounts_per_group : dict = {}
-    for stud in students:
-        amounts_per_group[stud.group] = 0
-    for stud in students:
-        amounts_per_group[stud.group] +=1
-    return amounts_per_group
+    amount_sets_P, rest_P = amount_student_P // k, amount_student_P % k
+    amount_sets_R, rest_R = amount_student_R // k, amount_student_R % k
 
-def create_last_group(amount_poo_group : dict, amount_rec_group : dict, amount_stud_per_group : dict, amount_rest_poo, amount_rest_rec, k):
-    for group in amount_stud_per_group:
-        try:
-            if min(amount_poo_group[group], amount_rest_poo) + min(amount_rec_group[group], amount_rest_rec) >= k:
-                return True
-        except:
-            continue
+    columns_to_v = []
+    all_fluctuations = []
+
+    for i in range(len(matrix[0])):
+            if matrix[0,i] + matrix[1,i] >= k:
+                columns_to_v.append([matrix[0,i], matrix[1,i]])
     
-    return False
+    for i in range(len(columns_to_v[0])):
+        fluctuations_i = []
+        for i in range(max(0, k - columns_to_v[1][i]), min(k, k - max(0, k - columns_to_v[0][i]))):
+            fluctuations_i.append(i)
+        all_fluctuations.append(fluctuations_i)
+    
+    list_of_posible_rest = [[0 for i in range(len(columns_to_v[0]))], 
+                            [0 for i in range(len(1,k))]]
+
+
+    for i in range(len(columns_to_v[0])):
+        for j in range(len(all_fluctuations[i])):
+            new_rest = k - all_fluctuations[i][j] + rest_P
+            list_of_posible_rest[i][new_rest] = 1
+    
+    for i in range(1,len(columns_to_v[0])):
+        for j in range(len(list_of_posible_rest[0])):
+            if list_of_posible_rest[i-1][j] == 1:
+
     
 
 
-
-def Solve(students : list[Student], k : int):
-    amountOfstud_per_group = count_stud_per_group(students)
-    susp_rec = []
-    susp_poo = []
-
-    for stud in students:
-        if stud.suspended == "P":
-            susp_poo.append(stud)
-        else:
-            susp_rec.append(stud)
-    
-    amountOfstud_per_group_rec = count_stud_per_group(susp_rec)
-    amountOfstud_per_group_poo = count_stud_per_group(susp_poo)
-    
-    susp_rec_grouped = group(susp_rec, k)
-    susp_poo_grouped = group(susp_poo, k)
-        
-    if (len(susp_rec_grouped) == 0 or len(susp_rec_grouped[len(susp_rec_grouped) - 1]) == k) and (len(susp_poo_grouped)==0 or len(susp_poo_grouped[len(susp_poo_grouped) - 1]) == k):
-        return len(susp_poo_grouped) + len(susp_rec_grouped)
-    else:
-        amount_rest_poo = get_amount_rest(susp_poo_grouped,k)
-        amount_rest_rec = get_amount_rest(susp_rec_grouped,k)
-
-        rest = 2 if amount_rest_rec > 0 and amount_rest_poo > 0 else 1
-
-        if amount_rest_rec + amount_rest_poo < k:
-            return len(susp_rec_grouped) + len(susp_poo_grouped) - rest
-        elif create_last_group(amountOfstud_per_group_poo,amountOfstud_per_group_rec, amountOfstud_per_group, amount_rest_poo, amount_rest_rec,k):
-            return len(susp_poo_grouped) + len(susp_rec_grouped) - 1
-        else:
-            return len(susp_poo_grouped) + len(susp_rec_grouped) - 2 
     
 def Solve_bruto(students : list[Student], k : int):
 
