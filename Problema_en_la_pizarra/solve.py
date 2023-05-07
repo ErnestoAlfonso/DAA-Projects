bfaa4a62d5dde323f565ed52336a40b9f815f0f7
jorgesolerrr <jorgeasoler2015@gmail.com>
2023-05-07 19:42:48 -0400

    finished

--- Problema_en_la_pizarra/solve.py
+++ Problema_en_la_pizarra/solve.py
@@ -1,4 +1,4 @@
-from tools import Graph
+from tools import BiGraph
 
 def board_problem_solve(n, tuples : list):
     graph = [[0] * n for i in range(n)]
@@ -6,7 +6,7 @@ def board_problem_solve(n, tuples : list):
         for i in range(t[0]-1, t[2]):
             graph[i][t[1]-1 : t[3]] = [1 for i in range(t[1]-1,t[3])]
     
-    BG = Graph(graph)
+    BG = BiGraph(graph)
     return BG.maxBPM()
 
-print(board_problem_solve(4, [(1,1,1,2), (2,1,2,1), (2,3,3,3)]))
+print(board_problem_solve(7, [(2,1,2,1), (4,2,4,3), (2,5,2,5), (2,3,5,3), (1,2,1,2), (3,2,5,3)]))
